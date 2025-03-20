375e81d4b348a8fcd897054445151f00833e075a
Elizabeth <132688916+eli12gran@users.noreply.github.com>
2025-03-19 23:25:59 -0500

    Inscription page (#8)
    * feat: create navbar
    
    * feat: Inscription css & tsx files created
    
    The Inscription css and tsx files were created along with the
    Speaker, Poster and Assistant forms and tsx files. Also the css file for
    the Speaker form. Still missing the css files for the poster and
    assistant forms.
    
    Contribution to issue #6
    
    * feat: add css files for the missing forms
    
    * feat: connected the home routes with React Router
    
    * feat: create the link to the home page from the navbar
    
    * fix:changes in the Home component, the body tag referred to the entire page, the Home was encapsulated in the body-container tag
    solution isso #7
    
    * feat: add & fix css forms files
    
    * feat: fix forms and navbar
    
    ---------
    
    Signed-off-by: Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
    Co-authored-by: Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
    Co-authored-by: Psychohistorian11 <cristianfrancoraigosa@gmail.com>

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,19 @@
+import Inscription from "./pages/Inscription.tsx";
 import Home from "./pages/Home.tsx"
 import Navbar from "./components/Navbar.tsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-    <Navbar />
-    <Home />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Inscription />} />
+      </Routes>
+    </Router >
+  );
 }
 
 export default App
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,40 +1,44 @@
-import './home.css'
-import Gmail_icon from "/assets/Gmail_Logo.png"
-import X_icon from "/assets/X.png"
-import Ig_icon from "/assets/Instagram_logo.png"
+import "./home.css";
+import { useNavigate } from "react-router-dom";
+import Gmail_icon from "/assets/Gmail_Logo.png";
+import X_icon from "/assets/X.png";
+import Ig_icon from "/assets/Instagram_logo.png";
 
-function Home(){
-    return (
-        <>
-  <h1>II Simposio de Química UdeM</h1>
-  <div className="button-container">
-    <a href="important-dates.html" className="btn">
-      Fechas Importantes
-    </a>
-    <a href="registration.html" className="btn">
-      Regístrate ahora
-    </a>
-    <a href="location.html" className="btn">
-      Ubicación y Direcciones
-    </a>
-  </div>
-  <div className="social-icons">
-    <a href="mailto:siquiudem@udemedellin.edu.co">
-      <img src={Gmail_icon} alt="Gmail" />
-    </a>
-    <a href="https://x.com/siquiudem" target="_blank">
-      <img src={X_icon} alt="X" />
-    </a>
-    <a
-      href="https://www.instagram.com/materialesconimpacto?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-      target="_blank"
-    >
-      <img src={Ig_icon} alt="Instagram" />
-    </a>
-  </div>
-</>
+function Home() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="body-container">
+        <h1>II Simposio de Química UdeM</h1>
+        <div className="button-container">
+          <button onClick={() => { }} className="btn">
+            Fechas Importantes
+          </button>
+          <button className="btn" onClick={() => navigate("/register")}>
+            Regístrate ahora
+          </button>
+          <button onClick={() => { }} className="btn">
+            Ubicación y Direcciones
+          </button>
+        </div>
 
-    );
+        <div className="social-icons">
+          <a href="mailto:siquiudem@udemedellin.edu.co">
+            <img src={Gmail_icon} alt="Gmail" />
+          </a>
+          <a href="https://x.com/siquiudem" target="_blank">
+            <img src={X_icon} alt="X" />
+          </a>
+          <a
+            href="https://www.instagram.com/materialesconimpacto?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+            target="_blank"
+          >
+            <img src={Ig_icon} alt="Instagram" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
